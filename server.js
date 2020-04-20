674de8792d33c81233f236cd030bb53557921856
shravaniM <shravani.manduva@laalsa.com>
2020-04-20 14:55:02 +0530

    postinstall & server file changed

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/noteapp'));
-
-// app.get('/*', function(req,res) {
-
-// res.sendFile(path.join(__dirname+'/dist/noteapp/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/noteapp'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/noteapp/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
